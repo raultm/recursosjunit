0f7fd2e713bafbd6cb3c39ee7524ec5b22106e36
raultm <raultm@gmail.com>
2024-04-24 10:11:05 +0200

    Asserts - notEquals, True y False

--- src/test/java/es/acaex/recursosjunit/raultm/AssertionsTest.java
+++ src/test/java/es/acaex/recursosjunit/raultm/AssertionsTest.java
@@ -1,24 +1,42 @@
 package es.acaex.recursosjunit.raultm;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import es.acaex.recursosjunit.MathUtils;
 
-public class AssertionsTest {
+class AssertionsTest {
     
     MathUtils mathUtils = new MathUtils();
     
     @BeforeEach
-    public void setUp() {
+    void setUp() {
         mathUtils = new MathUtils();
     }
 
     @Test 
-    public void testSuma() {
+    void checkAssertEquals() {
         assertEquals(5, mathUtils.suma(2, 3));
+    }
+
+    @Test
+    void checkAssertNotEquals(){
+        assertNotEquals(6, mathUtils.suma(2, 2));
+    }
+
+    @Test 
+    void checkAssertTrue(){
+        assertTrue(true);
+    }
+
+    @Test 
+    void checkAssertFalse(){
+        assertFalse(false);
     }
 
 }
