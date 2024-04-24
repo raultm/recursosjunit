1afeda1384c6db67805014c6f1e697827f45a6c4
raultm <raultm@gmail.com>
2024-04-24 09:35:30 +0200

    Recursos JUnit - Springboot Fresh Install

---
+++ src/main/java/es/acaex/recursosjunit/RecursosjunitApplication.java
@@ -0,0 +1,13 @@
+package es.acaex.recursosjunit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecursosjunitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RecursosjunitApplication.class, args);
+	}
+
+}
