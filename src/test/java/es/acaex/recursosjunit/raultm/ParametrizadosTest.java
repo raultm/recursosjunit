34ac89ba4c7cc09ba5c9743a969c277e0239817e
raultm <raultm@gmail.com>
2024-04-24 11:23:05 +0200

    Parametrizacion con Metodo

--- src/test/java/es/acaex/recursosjunit/raultm/ParametrizadosTest.java
+++ src/test/java/es/acaex/recursosjunit/raultm/ParametrizadosTest.java
@@ -1,9 +1,14 @@
 package es.acaex.recursosjunit.raultm;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import java.util.stream.Stream;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 import org.junit.jupiter.params.provider.ValueSource;
 
 import es.acaex.recursosjunit.MathUtils;
@@ -21,6 +26,20 @@ public class ParametrizadosTest {
     @ValueSource(ints = { 2, 7, 29, 67, 89 })
     void checkValueSource(int numberToCheck){
         assertTrue(mathUtils.isPrime(numberToCheck));
+    }
+
+    @ParameterizedTest
+    @MethodSource("sumas")
+    void checkMethodSource(int sum1, int sum2, int sumTotal){
+        assertEquals(sumTotal, mathUtils.suma(sum1, sum2));
+    }
+
+    static Stream<Arguments> sumas(){
+        return Stream.of(
+            Arguments.of(2, 3, 5),
+            Arguments.of(5, 7, 12),
+            Arguments.of(-3, 8, 5)
+        );
     }
 
 }
