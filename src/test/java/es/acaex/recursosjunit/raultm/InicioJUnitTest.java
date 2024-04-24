f4d8f043082119dabaf2b39b022559e363272279
raultm <raultm@gmail.com>
2024-04-24 09:57:27 +0200

    Organizacion de una Clase de Test

--- src/test/java/es/acaex/recursosjunit/raultm/InicioJUnitTest.java
+++ src/test/java/es/acaex/recursosjunit/raultm/InicioJUnitTest.java
@@ -2,21 +2,44 @@ package es.acaex.recursosjunit.raultm;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import es.acaex.recursosjunit.MathUtils;
 
 public class InicioJUnitTest {
     
+    MathUtils mathUtils;
+
+    @BeforeAll
+    public static void setUpAll(){
+        // Configuración inicial de la clase
+    }
+
+    @AfterAll
+    public static void tearDownAll(){
+        // Limpieza después de todas las pruebas
+    }
+
+    @BeforeEach
+    public void setUp() {
+        // Configuración inicial antes de cada prueba
+        mathUtils = new MathUtils();
+    }
+
+    @AfterEach
+    public void tearDown() {
+        // Limpieza después de cada prueba
+        mathUtils = null;
+    }
+
     @Test
     public void testSuma() {
-        // Arrange
-        MathUtils mathUtils = new MathUtils();
-
-        // Act
         int resultado = mathUtils.suma(2, 3);
 
-        // Assert
         assertEquals(5, resultado);
     }
 }
