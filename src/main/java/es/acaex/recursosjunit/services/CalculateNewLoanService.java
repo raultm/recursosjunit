03d7b5ae130d1c537a328766210c1ebf98e9f2a5
raultm <raultm@gmail.com>
2024-04-26 08:56:51 +0200

    Calcular prestamos - Refactor

--- src/main/java/es/acaex/recursosjunit/services/CalculateNewLoanService.java
+++ src/main/java/es/acaex/recursosjunit/services/CalculateNewLoanService.java
@@ -9,40 +9,49 @@ import es.acaex.recursosjunit.models.Member;
 public class CalculateNewLoanService {
 
     public Loan execute(Member member, Copy copy, String dateString) {
-        int loanDays = 21;
-        if(!copy.isBorrowed()){
-            if(!member.isLoanLimitReached()){
-                if(member.getAge() >= copy.getRecommendedAge()){
-                    if(  copy.getReservedBy() == null
-                      || copy.getReservedBy().equals(member.getUsername())
-                    ){
-                        if(copy.isNew()){
-                            loanDays = 14;
-                        }else{
-                            if(member.isPremium()){
-                                loanDays = 35;
-                            }
-                        }
-                
-                        return Loan.builder()
-                                .member(member)
-                                .copy(copy)
-                                .startedAt(LocalDate.parse(dateString))
-                                .expiredAt(LocalDate.parse(dateString).plusDays(loanDays))
-                                .build();
-                    }else{
-                        throw new RuntimeException("La copia ya está reservada");
-                    }
-                }else{
-                    throw new RuntimeException("La Copia no se puede prestar a ese Socio por la edad");
-                }
-            }else{
-                throw new RuntimeException("El Socio ha alcanzado el límite de préstamos abiertos");
-            }
-        }else{
-            throw new RuntimeException("La copia ya está prestada");
-        }
+        
+        validacionesQueImpidenElPrestamo(member, copy);
+        
+        return Loan.builder()
+                .member(member)
+                .copy(copy)
+                .startedAt(LocalDate.parse(dateString))
+                .expiredAt(LocalDate.parse(dateString).plusDays(getLoanDays(member, copy)))
+                .build();
         
     }
+
+    private int getLoanDays(Member member, Copy copy) {
+        int loanDays = 21;
+        
+        if(copy.isNew()){
+            loanDays = 14;
+        }else{
+            if(member.isPremium()){
+                loanDays = 35;
+            }
+        }
+        return loanDays;
+    }
+
+    private void validacionesQueImpidenElPrestamo(Member member, Copy copy) {
+        if(copy.isBorrowed()){
+            throw new RuntimeException("La copia ya está prestada");
+        }
+
+        if(member.isLoanLimitReached()){
+            throw new RuntimeException("El Socio ha alcanzado el límite de préstamos abiertos");
+        }
+
+        if(member.getAge() < copy.getRecommendedAge()){
+            throw new RuntimeException("La Copia no se puede prestar a ese Socio por la edad");
+        }
+
+        if(  copy.getReservedBy() != null && !copy.getReservedBy().equals(member.getUsername())){
+            throw new RuntimeException("La copia ya está reservada");
+        }
+    }
+        
+    
     
 }
